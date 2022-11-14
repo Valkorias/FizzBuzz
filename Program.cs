049ffba649792b55c60254ccbf529d1e3f0a1e94
David Figone <david.louis.figone@gmail.com>
2022-11-14 12:28:44 -0600

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        int i;
+
+
+        Console.WriteLine("Please type a number: ");
+        i = int.Parse(Console.ReadLine());
+
+        if ((i % 3) == 0 && (i % 5) == 0)
+        {
+            Console.WriteLine("FizzBuzz");
+        }
+        else if ((i % 3) == 0)
+        {
+            Console.WriteLine("Fizz");
+        }
+        else if ((i % 5) == 0)
+        {
+            Console.WriteLine("Buzz");
+        }
+        else
+        {
+            Console.WriteLine(i);
+        }
+
+    }
+}
